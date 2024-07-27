3a95fceeb4f80443a08e5c4f3196f95b063931a1
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-07-27 17:01:18 +0200

    fix(controller): removed warnings

--- controller/src/pipeline/pipeline_controller.rs
+++ controller/src/pipeline/pipeline_controller.rs
@@ -1,10 +1,10 @@
-use std::{borrow::Borrow, fmt::Display, io::Read, sync::Arc};
+use std::{io::Read, sync::Arc};
 
 use actix_multipart::form::{tempfile::TempFile, MultipartForm};
 use actix_web::{http::StatusCode, post, web, HttpResponse, Responder};
 use tracing::info;
 
-use crate::parser::pipe_parser::{ManifestParser, MockManifestParser, ParsingError};
+use crate::parser::pipe_parser::{ManifestParser, MockManifestParser};
 
 #[derive(Debug, MultipartForm)]
 struct UploadPipelineForm {
@@ -20,7 +20,7 @@ pub async fn create_pipeline(
     let result = parser.parse("".to_string()).expect("c'est mocké");
     info!("Uploaded file {}", form.file.size);
     let f = form.file;
-    let path = format!("./tmp/{}", f.file_name.unwrap());
+    let _path = format!("./tmp/{}", f.file_name.unwrap());
     let mut fd_manifest = f.file.reopen().unwrap(); //TODO: handle this error
     let mut buffer = String::new();
     match fd_manifest.read_to_string(&mut buffer) {
