962926028714631cf61f99588c5df05bd389c48d
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-08-19 23:12:41 +0200

    feat(register): adding simple way to register
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,47 +1,23 @@
-use std::{error::Error};
-
-
+use registering_service::register_agent;
+use std::error::Error;
+mod registering_service;
 mod proto {
     tonic::include_proto!("scheduler");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let url = "http://[::1]:5001";
-    let mut client: proto::agent_client::AgentClient<tonic::transport::Channel>;
-    let mut tries = 0;
-    while tries != 5 {  // tries < 5 ???
-        println!("Try number: {}", tries);
-        client = match proto::agent_client::AgentClient::connect(url).await {
-            Ok(mut cli) => {
-                println!("Connection succeeded");
-                let req = proto::Health {
-                    cpu_usage: 1,
-                    memory_usage: 1,
-                };
-                let request = tonic::Request::new(req);
-                cli.register_agent(request).await.unwrap();
-                cli
-            }
-            Err(err) => {
-                if tries == 4 {
-                    println!("Connection failed: {:?}", err);
-                    return Ok(());
-                } else {
-                    println!("Connection failed: {:?}", err);
-                    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    let args: Vec<String> = std::env::args().collect();
+    // "http://[::1]:5001"
+    let scheduler_url = &args[1];
 
-                    tries += 1;
-                    continue;
-                }
-            }
-        };
-        return Ok(());
-
-    }
-
-    // stop the script if the connection is not established
-
+    match register_agent(&scheduler_url).await {
+        Ok(_) => println!("Connection succeeded"),
+        Err(err) => {
+            println!("Connection failed: {:?}", err);
+            return Ok(());
+        }
+    };
+    println!("Starting server...");
     Ok(())
 }
-
