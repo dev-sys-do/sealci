6c66c49e30a9987df5e951b29d7a9f27813734c8
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-20 23:26:14 +0200

    refacto Scheduler architecture
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile(&["api/proto/scheduler/agent.proto","api/proto/scheduler/controller.proto"], &["api/proto/scheduler"])
-        .unwrap();
-}
