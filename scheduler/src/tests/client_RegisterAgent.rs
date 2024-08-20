f084c6a803fbe9bafdacc20cd64ec39a4d5bef38
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-21 01:22:37 +0200

    chore: rename files
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

---
+++ scheduler/src/tests/client_RegisterAgent.rs
@@ -0,0 +1,21 @@
+use proto::agent_client::AgentClient;
+use std::error::Error;
+
+pub mod proto {
+	tonic::include_proto!("scheduler");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+	let url = "http://[::1]:50051";
+	let mut client = AgentClient::connect(url).await?;
+
+	let req = proto::Health { cpu_usage: 123, memory_usage: 321 };
+	let request = tonic::Request::new(req);
+
+	let response = client.register_agent(request).await?;
+
+	println!("Response: {:?}", response.get_ref());
+
+	Ok(())
+}
