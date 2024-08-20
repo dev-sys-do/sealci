9c3fe5fb21f143e6f5b5d00eca406f1b2497dbce
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-20 23:17:19 +0200

    feat(agent registration): added registration test + readme
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

---
+++ scheduler/README.md
@@ -0,0 +1,21 @@
+# Scheduler technical documentation
+
+## Dependencies
+
+This microservice uses Tonic. Tonic makes use of Prost. [...]
+
+As of this commit, no AI was used to make the scheduler! :D (yet D:)
+
+## Commands
+
+Starting the Scheduler server
+
+```bash
+cargo run --bin server
+```
+
+Testing Agent registration
+
+```bash
+cargo run --bin client_register_agent
+```
--- scheduler/src/client_register_agent.rs
+++ scheduler/src/client_register_agent.rs
@@ -1,21 +1,21 @@
-use proto::scheduler_client::SchedulerClient;
+use proto::agent_client::AgentClient;
 use std::error::Error;
 
 pub mod proto {
-	tonic::include_proto!("agent");
+	tonic::include_proto!("scheduler");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 	let url = "http://[::1]:50051";
-	let mut client = SchedulerClient::connect(url).await?;
+	let mut client = AgentClient::connect(url).await?;
 
-	let req = proto::SchedulerRequest { a: 4, b: 5 };
+	let req = proto::Health { cpu_usage: 123, memory_usage: 321 };
 	let request = tonic::Request::new(req);
 
-	let response = client.add(request).await?;
+	let response = client.register_agent(request).await?;
 
-	println!("Response: {:?}", response.get_ref().result);
+	println!("Response: {:?}", response.get_ref());
 
 	Ok(())
 }
