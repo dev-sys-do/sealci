53702fa035db3d3e1377914cd76b3f590dc0e148
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-22 00:39:06 +0200

    feat(gRPC controller interface): added controller service to server
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,14 +1,17 @@
 use tonic::transport::Server;
 mod proto;
 use proto::agent_server::AgentServer;
+use proto::controller_server::ControllerServer;
 mod interfaces;
 use interfaces::agent_interface::AgentService;
+use interfaces::controller_interface::ControllerService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let addr = "[::1]:50051".parse()?;
 
 	let agent = AgentService::default();
+	let controller = ControllerService::default();
 
 	let service = tonic_reflection::server::Builder::configure()
 		.register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
@@ -17,6 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	Server::builder()
 		.add_service(service)
 		.add_service(AgentServer::new(agent))
+		.add_service(ControllerServer::new(controller))
 		.serve(addr)
 		.await?;
 
