6a2d4c644c25c5927f0bf4eedb0057da7c301b62
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-22 15:45:01 +0200

    feat(int tests): make interfaces/ and proto/ available as an external lib outside the package
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- scheduler/src/proto/mod.rs
+++ scheduler/src/proto/mod.rs
@@ -1,4 +1,4 @@
 tonic::include_proto!("scheduler");
 
-pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+pub const FILE_DESCRIPTOR_SET: &[u8] =
     tonic::include_file_descriptor_set!("scheduler_descriptor");
