ce2598f05dd63bc1e62c25eeaa886f984805e536
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-24 17:05:54 +0200

    feat(logging): added logging in main and interfaces
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,13 +1,19 @@
+use env_logger;
+use log::info;
 use tonic::transport::Server;
+
 mod proto;
 use proto::agent_server::AgentServer;
 use proto::controller_server::ControllerServer;
+
 mod interfaces;
 use interfaces::agent_interface::AgentService;
 use interfaces::controller_interface::ControllerService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	env_logger::init();
+
 	let addr = "[::1]:50051".parse()?;
 
 	let agent = AgentService::default();
@@ -17,6 +23,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		.register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
 		.build()?;
 
+  info!("Starting gRPC server at {}", addr);
 	Server::builder()
 		.add_service(service)
 		.add_service(AgentServer::new(agent))
