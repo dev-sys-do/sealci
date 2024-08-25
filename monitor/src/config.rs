f1f4f678646acab504d64f9daa1588863831a6ff
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-08-25 14:12:00 +0200

    add pull request verifications
    Signed-off-by: Baptiste Bronsin <baptiste.bronsin@outlook.com>

--- monitor/src/config.rs
+++ monitor/src/config.rs
@@ -1,9 +1,11 @@
 use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
+    pub event: String,
     pub repo_owner: String,
     pub repo_name: String,
     pub github_token: String,
@@ -16,7 +18,13 @@ impl Config {
         let mut contents = String::new();
         file.read_to_string(&mut contents).expect("Failed to read config file");
 
-        let config: Config = serde_yaml::from_str(&contents).expect("Failed to parse YAML");
+        let config: Config = serde_yaml::from_str(&mut contents).expect("Failed to parse YAML");
+
+        // Verify that the actions file exists
+        if !Path::new(&config.actions_path).exists() {
+            panic!("The actions file '{}' does not exist.", config.actions_path);
+        }
+
         config
     }
 }
--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -3,7 +3,7 @@ mod event_listener;
 mod controller;
 
 use crate::config::Config;
-use crate::event_listener::listen_to_commits;
+use crate::event_listener::{get_github_repo_url, listen_to_commits, listen_to_pull_requests};
 use crate::controller::send_to_controller;
 use clap::{Arg, Command};
 use std::path::Path;
@@ -21,6 +21,11 @@ fn main() {
             .long("config")
             .required(false)
             .help("The path to the config file"))
+        .arg(Arg::new("event")
+            .short('e')
+            .long("event")
+            .required(false)
+            .help("The event to listen to (commit, pull_request, *)"))
         .arg(Arg::new("repo_owner")
             .long("owner")
             .required(false)
@@ -45,6 +50,7 @@ fn main() {
     } else {
         println!("-- SealCI - Loading config from CLI arguments");
         Config {
+            event: matches.get_one::<String>("event").expect("--event argument is required").to_string(),
             repo_owner: matches.get_one::<String>("repo_owner").expect("--owner argument is required").to_string(),
             repo_name: matches.get_one::<String>("repo_name").expect("--repo argument is required").to_string(),
             github_token: matches.get_one::<String>("github_token").expect("--token argument is required").to_string(),
@@ -55,23 +61,43 @@ fn main() {
     println!("-- SealCI - Config loaded !");
     println!("{:#?}", config);
 
-    // Emprunter `config` par référence
+    // Borrowing the config by reference
     let config: Arc<Config> = Arc::new(config);
 
-    // Initialisation du runtime en dehors de la fermeture
-    let rt: Runtime = Runtime::new().expect("Failed to create runtime");
+    
+    if config.event == "commit" || config.event == "*" {
+        let rt: Runtime = Runtime::new().expect("Failed to create runtime");
 
-    listen_to_commits(&config.repo_owner, &config.repo_name, &config.github_token, {
-        let config: Arc<Config> = Arc::clone(&config);
-        let repo_url: String = format!("https://github.com/repos/{}/{}", config.repo_owner, config.repo_name);
-        
-        move || {
-            rt.block_on(async {
-                match send_to_controller("pipeline_name", &repo_url, Path::new(&config.actions_path)).await {
-                    Ok(_) => println!("Pipeline sent successfully"),
-                    Err(e) => eprintln!("Failed to send pipeline: {}", e),
-                }
-            });
-        }
-    });
+        listen_to_commits(&config, {
+            let config: Arc<Config> = Arc::clone(&config);
+            let repo_url: String = get_github_repo_url(&config.repo_owner, &config.repo_name);
+            
+            move || {
+                rt.block_on(async {
+                    match send_to_controller("pipeline_name", &repo_url, Path::new(&config.actions_path)).await {
+                        Ok(_) => println!("Pipeline sent successfully"),
+                        Err(e) => eprintln!("Failed to send pipeline: {}", e),
+                    }
+                });
+            }
+        });
+    }
+    if config.event == "pull_request" || config.event == "*" {
+        let rt: Runtime = Runtime::new().expect("Failed to create runtime");
+
+        listen_to_pull_requests(&config, {
+            let config: Arc<Config> = Arc::clone(&config);
+            let repo_url: String = get_github_repo_url(&config.repo_owner, &config.repo_name);
+            
+            move || {
+                rt.block_on(async {
+                    match send_to_controller("pipeline_name", &repo_url, Path::new(&config.actions_path)).await {
+                        Ok(_) => println!("Pipeline sent successfully"),
+                        Err(e) => eprintln!("Failed to send pipeline: {}", e),
+                    }
+                });
+            }
+        });
+    }
+    
 }
