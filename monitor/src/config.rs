41d72118518ddc194d060f40c4a7fb966d2c7b34
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-08-26 15:08:29 +0200

    add multiconfigurations'
    Signed-off-by: Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
    Signed-off-by: pauline <pauline.contat00@gmail.com>

--- monitor/src/config.rs
+++ monitor/src/config.rs
@@ -4,12 +4,17 @@ use std::io::Read;
 use std::path::Path;
 
 #[derive(Deserialize, Debug, Default)]
-pub struct Config {
+pub struct SingleConfig {
     pub event: String,
     pub repo_owner: String,
     pub repo_name: String,
     pub github_token: String,
     pub actions_path: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub configurations: Vec<SingleConfig>,
 }
 
 impl Config {
@@ -20,15 +25,17 @@ impl Config {
 
         let config: Config = serde_yaml::from_str(&mut contents).expect("Failed to parse YAML");
 
-        // Verify that the actions file exists
-        Self::exists_actions_file(&config);
+        // Verify that the actions files exist for each configuration
+        for single_config in &config.configurations {
+            single_config::exists_actions_file(&config);
+        }
 
         config
     }
 
-    pub(crate) fn exists_actions_file(config: &Config) {
+    pub(crate) fn exists_actions_file(config: &SingleConfig) {
         if !Path::new(&config.actions_path).exists() {
-            panic!("The actions file '{}' does not exist.", config.actions_path);
+            panic!("The actions file '{}' for repo '{}' does not exist.", config.actions_path, config.repo_name);
         }
     }
 }
