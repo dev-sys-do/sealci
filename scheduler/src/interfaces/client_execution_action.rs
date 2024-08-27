44b038e3854b33142aa290b38ca7a453f7da003f
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-08-27 13:07:58 +0200

    feat: Add test for scheduling client execution action
    The commit adds a new test `test_schedule_action` in the `client_execution_action.rs` file. This test is responsible for scheduling a client execution action using the `ControllerClient` and asserting the response action ID. This test ensures the proper functioning of the scheduling feature and improves the overall code quality.
    
    Signed-off-by: benoit-planche <benoit.planche@etu.umontpellier.fr>

---
+++ scheduler/src/interfaces/client_execution_action.rs
@@ -0,0 +1,34 @@
+use scheduler::proto::{controller_client::ControllerClient, ActionRequest, ExecutionContext, RunnerType};
+use scheduler::proto::agent_server::AgentServer;
+use scheduler::proto::controller_server::ControllerServer;
+use scheduler::interfaces::agent_interface::AgentService;
+use scheduler::interfaces::controller_interface::ControllerService;
+use tonic::transport::Server;
+use tonic::transport::Channel;
+use tonic::Request;
+use std::error::Error;
+use tokio::time::Duration;
+
+#[tokio::test]
+async fn test_schedule_action() -> Result<(), Box<dyn Error>> {
+    let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+    let mut client = ControllerClient::new(channel);
+
+    let request = Request::new(ActionRequest {
+        action_id: "test_action_id".to_string(),
+        context: Some(ExecutionContext {
+            r#type: RunnerType::Docker.into(),
+            container_image: Some("test_image".to_string()),
+        }),
+        commands: vec!["echo".to_string(), "Hello, World!".to_string()],
+    });
+
+    let mut response_stream = client.schedule_action(request).await?.into_inner();
+
+    while let Some(response) = response_stream.message().await? {
+        // Modify the assertion based on the correct field available in your response
+        assert_eq!(response.action_id, "mock_action_id");
+    }
+
+    Ok(())
+}
