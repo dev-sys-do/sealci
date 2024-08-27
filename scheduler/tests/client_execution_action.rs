ad0aaf8e71b3f0b7ab364c073b8e200ff999060f
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-27 17:08:39 +0200

    feat(controller client): implementing controller client
    Co-authored-by: benoit-planche <benoit.planche@etu.umontpellier.fr>
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- scheduler/tests/client_execution_action.rs
+++ scheduler/tests/client_execution_action.rs
@@ -1,26 +1,20 @@
-use scheduler::proto::{controller_client::ControllerClient, ActionRequest, ExecutionContext, RunnerType};
-use scheduler::proto::agent_server::AgentServer;
-use scheduler::proto::controller_server::ControllerServer;
-use scheduler::interfaces::agent_interface::AgentService;
-use scheduler::interfaces::controller_interface::ControllerService;
-use tonic::transport::Server;
+use scheduler::proto::{actions_client::ActionsClient, ActionRequest, ExecutionContext, RunnerType};
 use tonic::transport::Channel;
 use tonic::Request;
 use std::error::Error;
-use tokio::time::Duration;
 
 #[tokio::test]
 async fn test_execution_action() -> Result<(), Box<dyn Error>> {
     let channel = Channel::from_static("http://[::1]:50051").connect().await?;
-    let mut client = ControllerClient::new(channel);
+    let mut client = ActionsClient::new(channel);
 
     let request = Request::new(ActionRequest {
-        action_id: "test_action_id".to_string(),
+        action_id: String::from("azuyr-91881FZ68fz681ZF-f"),
         context: Some(ExecutionContext {
             r#type: RunnerType::Docker.into(),
-            container_image: Some("test_image".to_string()),
+            container_image: Some(String::from("test_image")),
         }),
-        commands: vec!["echo".to_string(), "Hello, World!".to_string()],
+        commands: vec![String::from("echo 'salut les agents\n\n\n\n          O\\ w /O\n\n'"), String::from("shutdown now")],
     });
 
     let mut response_stream = client.execution_action(request).await?.into_inner();
