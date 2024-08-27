57b52e418d35cdfd006496c1c20e7e10ec872919
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-27 21:57:30 +0200

    refactor(proto): update build.rs & src/proto/ to add new proto and subpackages
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- scheduler/src/proto/mod.rs
+++ scheduler/src/proto/mod.rs
@@ -1,4 +1,17 @@
-tonic::include_proto!("scheduler");
+// Include the generated code for the `scheduler.agent` proto package
+pub mod agent {
+    tonic::include_proto!("scheduler.agent");
+}
+
+// Include the generated code for the `scheduler.controller` proto package
+pub mod controller {
+    tonic::include_proto!("scheduler.controller");
+}
+
+// Include the generated code for the `scheduler.controller` proto package
+pub mod actions {
+    tonic::include_proto!("scheduler.actions");
+}
 
 pub const FILE_DESCRIPTOR_SET: &[u8] =
     tonic::include_file_descriptor_set!("scheduler_descriptor");
