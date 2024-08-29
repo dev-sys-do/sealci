8f6dfee1a0b3af1386ebebfbb8c4ec3d530dafde
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-08-29 15:55:25 +0200

    wip(controller): added repo_url to api and proto

--- controller/src/pipeline/pipeline_service.rs
+++ controller/src/pipeline/pipeline_service.rs
@@ -1,19 +1,19 @@
-use std::sync::Arc;
 use sqlx::PgPool;
+use std::sync::Arc;
 
 use tokio::task;
 use tracing::info;
 
+use crate::pipeline::pipeline_repository::PipelineRepository;
 use crate::{
     parser::pipe_parser::{Action, ManifestParser, ParsingError, Pipeline},
     scheduler::SchedulerService,
 };
-use crate::pipeline::pipeline_repository::PipelineRepository;
 
 pub struct PipelineService {
     client: Arc<SchedulerService>,
     parser: Arc<dyn ManifestParser>,
-    repository: Arc<PipelineRepository>
+    repository: Arc<PipelineRepository>,
 }
 
 #[derive(Debug)]
@@ -23,20 +23,33 @@ pub enum PipelineServiceError {
 }
 
 impl PipelineService {
-    pub fn new(client: Arc<SchedulerService>, parser: Arc<dyn ManifestParser>, pool: Arc<PgPool>) -> Self {
+    pub fn new(
+        client: Arc<SchedulerService>,
+        parser: Arc<dyn ManifestParser>,
+        pool: Arc<PgPool>,
+    ) -> Self {
         let repository = Arc::new(PipelineRepository::new(pool.clone()));
-        Self { client, parser, repository }
+        Self {
+            client,
+            parser,
+            repository,
+        }
     }
 
     pub fn try_parse_pipeline(&self, manifest: String) -> Result<Pipeline, ParsingError> {
         self.parser.parse(manifest)
     }
 
-    pub async fn send_actions(&self, pipeline: Pipeline) -> Result<(), PipelineServiceError> {
+    pub async fn send_actions(
+        &self,
+        pipeline: Pipeline,
+        repo_url: String,
+    ) -> Result<(), PipelineServiceError> {
         let client = Arc::clone(&self.client);
         for action in pipeline.actions {
             info!("Sending action: {:?}", action);
-            self.send_action(client.clone(), Arc::new(action)).await?;
+            self.send_action(client.clone(), Arc::new(action), repo_url.clone())
+                .await?;
         }
         Ok(())
     }
@@ -45,9 +58,10 @@ impl PipelineService {
         &self,
         client: Arc<SchedulerService>,
         action: Arc<Action>,
+        repo_url: String,
     ) -> Result<(), PipelineServiceError> {
         task::spawn(async move {
-            client.send_action(action).await.unwrap();
+            client.send_action(action, repo_url).await.unwrap();
         });
         Ok(())
     }
