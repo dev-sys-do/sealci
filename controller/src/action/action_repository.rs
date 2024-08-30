7f8bbdfdd58cbf803a3d08da37daf3db6eff023d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-08-30 15:19:52 +0200

    feat: Add find_by_id and find_by_pipeline_id methods to ActionRepository
    The commit adds the `find_by_id` and `find_by_pipeline_id` methods to the `ActionRepository` in the `action_repository.rs` file. These methods allow for retrieving actions from the database based on their ID or pipeline ID. This functionality enhances the flexibility and querying capabilities of the repository.
    
    Note: This commit message follows the established convention of using the "feat" type for new features or functionality additions.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>
    Signed-off-by: Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>

--- controller/src/action/action_repository.rs
+++ controller/src/action/action_repository.rs
@@ -36,4 +36,20 @@ impl ActionRepository {
         .await
         .unwrap();
     }
+
+    pub async fn find_by_id(&self, id: i64) -> Result<Action, sqlx::Error> {
+        let row = sqlx::query_as!(Action, r#"SELECT * FROM actions WHERE id = $1"#, id)
+            .fetch_one(&*self.pool)
+            .await?;
+
+        Ok(row)
+    }
+
+    pub async fn find_by_pipeline_id(&self, pipeline_id: i64) -> Result<Vec<Action>, sqlx::Error> {
+        let rows = sqlx::query_as!(Action, r#"SELECT * FROM actions WHERE pipeline_id = $1"#, pipeline_id)
+            .fetch_all(&*self.pool)
+            .await?;
+
+        Ok(rows)
+    }
 }
