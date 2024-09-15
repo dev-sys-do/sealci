c23e8f755dbc9881fbcf3f92968ab8af3b1384d3
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-09-15 19:37:34 +0200

    feat(errors):handling in api
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
    Signed-off-by: Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>

--- monitor/src/external_api.rs
+++ monitor/src/external_api.rs
@@ -4,7 +4,7 @@ use crate::file_utils::process_multipart_form;
 use crate::thread_utils::{manage_threads, RequestType};
 use actix_multipart::Multipart;
 use actix_web::web::Data;
-use actix_web::{web, Error, HttpResponse, Responder, Result};
+use actix_web::{delete, get, post, put, web, Error, HttpResponse, Responder, Result};
 use serde::Serialize;
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -23,7 +23,7 @@ pub struct ConfigWithId {
     github_token: String,
     actions_path: String,
 }
-
+#[get("/configurations")]
 pub async fn get_configurations(data: web::Data<AppState>) -> impl Responder {
     let configs = data.configs.read().await;
 
@@ -44,77 +44,89 @@ pub async fn get_configurations(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(configs_with_id)
 }
 
+#[get("/configurations/{id}")]
 pub async fn get_configuration_by_id(
     data: web::Data<AppState>,
     path: web::Path<usize>,
-) -> Result<impl Responder, Error> {
+) -> impl Responder {
     let id = path.into_inner();
     let configs = data.configs.read().await;
 
     if id >= 1 && id <= configs.configurations.len() {
         let config = &configs.configurations[id - 1];
-        Ok(HttpResponse::Ok().json(config))
+        HttpResponse::Ok().json(config)
     } else {
-        Ok(HttpResponse::NotFound().body(CONFIG_NOT_FOUND))
+        HttpResponse::NotFound().body(CONFIG_NOT_FOUND)
     }
 }
 
+#[post("/configurations")]
 pub async fn add_configuration(
     payload: Multipart,
     data: Data<AppState>,
     thread_list: Data<RwLock<JoinSet<()>>>,
-) -> Result<HttpResponse, Error> {
-    let result = process_multipart_form(payload).await?;
+) -> impl Responder {
+    let result = match process_multipart_form(payload).await {
+        Ok(result) => result,
+        Err(e) => return HttpResponse::BadRequest().json(e.to_string()),
+    };
+    let actions_path = match result.actions_file_path {
+        Some(actions_path) => actions_path,
+        None => return HttpResponse::BadRequest().json(MISSING_CONFIG),
+    };
+    let single_config = SingleConfig {
+        event: result.new_config.event,
+        repo_owner: result.new_config.repo_owner,
+        repo_name: result.new_config.repo_name,
+        github_token: result.new_config.github_token,
+        actions_path,
+    };
 
-    if let Some(actions_path) = result.actions_file_path {
-        let single_config = SingleConfig {
-            event: result.new_config.event,
-            repo_owner: result.new_config.repo_owner,
-            repo_name: result.new_config.repo_name,
-            github_token: result.new_config.github_token,
-            actions_path,
-        };
+    let configs = data.configs.read().await;
+    let existing_config = configs.configurations.iter().any(|config| {
+        config.repo_owner == single_config.repo_owner
+            && config.repo_name == single_config.repo_name
+            && (config.event == single_config.event
+                || config.event == "*"
+                || single_config.event == "*")
+    });
 
-        {
-            let configs = data.configs.read().await;
-            let existing_config = configs.configurations.iter().any(|config| {
-                config.repo_owner == single_config.repo_owner &&
-                    config.repo_name == single_config.repo_name &&
-                    (config.event == single_config.event || config.event == "*" || single_config.event == "*")
-            });
-
-            if existing_config {
-                return Err(actix_web::error::ErrorBadRequest(
-                    "Configuration for this repository and event already exists",
-                ));
-            }
-        }
-
-        {
-            let mut configs = data.configs.write().await;
-            configs.configurations.push(single_config.clone());
-            configs.save_to_file().await.map_err(|e| {
-                actix_web::error::ErrorInternalServerError(format!("Failed to save config: {}", e))
-            })?;
-        }
-
-        manage_threads(&RequestType::Create, thread_list.clone(), Data::from(Arc::clone(&data.configs))).await;
-
-        Ok(HttpResponse::Ok().json(single_config))
-    } else {
-        Err(actix_web::error::ErrorBadRequest(MISSING_CONFIG))
+    if existing_config {
+        return HttpResponse::BadRequest()
+            .json("Configuration for this repository and event already exists");
     }
+
+    drop(configs); // Release read lock
+
+    let mut configs = data.configs.write().await;
+    configs.configurations.push(single_config.clone());
+    let _ = configs.save_to_file().map_err(|e| {
+        return HttpResponse::UnprocessableEntity().json(format!("Failed to save config: {}", e));
+    });
+
+    manage_threads(
+        &RequestType::Create,
+        thread_list.clone(),
+        Data::from(Arc::clone(&data.configs)),
+    )
+    .await;
+
+    HttpResponse::Ok().json(single_config)
 }
 
+#[put("/configurations/{id}")]
 pub async fn update_configuration(
     data: Data<AppState>,
     payload: Multipart,
     path: web::Path<usize>,
     thread_list: Data<RwLock<JoinSet<()>>>,
-) -> Result<HttpResponse, Error> {
+) -> impl Responder {
     let id = path.into_inner();
     // Process the form payload before updating the configuration
-    let result = process_multipart_form(payload).await?;
+    let result = match process_multipart_form(payload).await {
+        Ok(result) => result,
+        Err(e) => return HttpResponse::BadRequest().json(e.to_string()),
+    };
 
     // Lock the configs, update, and save changes
     let updated_config = {
@@ -122,7 +134,7 @@ pub async fn update_configuration(
 
         // Check if the configuration exists
         if id < 1 || id > configs.configurations.len() {
-            return Ok(HttpResponse::NotFound().body(CONFIG_NOT_FOUND));
+            return HttpResponse::NotFound().body(CONFIG_NOT_FOUND);
         }
 
         // Borrow the config mutably
@@ -138,25 +150,32 @@ pub async fn update_configuration(
         }
 
         let updated_config = config.clone();
-        configs.save_to_file().await.map_err(|e| {
-            actix_web::error::ErrorInternalServerError(format!("Failed to save config: {}", e))
-        })?;
+        let _ = configs.save_to_file().map_err(|e| {
+            return HttpResponse::UnprocessableEntity()
+                .json(format!("Failed to save config: {}", e));
+        });
 
         updated_config
     };
 
     // Restart threads with the updated configuration after lock release
-    manage_threads(&RequestType::Update, thread_list.clone(), Data::from(Arc::clone(&data.configs))).await;
+    manage_threads(
+        &RequestType::Update,
+        thread_list.clone(),
+        Data::from(Arc::clone(&data.configs)),
+    )
+    .await;
 
     // Return the updated config
-    Ok(HttpResponse::Ok().json(updated_config))
+    HttpResponse::Ok().json(updated_config)
 }
 
+#[delete("/configurations/{id}")]
 pub async fn delete_configuration(
     data: Data<AppState>,
     path: web::Path<usize>,
     thread_list: Data<RwLock<JoinSet<()>>>,
-) -> Result<impl Responder, Error> {
+) -> impl Responder {
     let id = path.into_inner();
 
     let removed_config = {
@@ -164,28 +183,32 @@ pub async fn delete_configuration(
 
         // Check if the configuration exists
         if id < 1 || id > configs.configurations.len() {
-            return Ok(HttpResponse::NotFound().body(CONFIG_NOT_FOUND));
+            return HttpResponse::NotFound().body(CONFIG_NOT_FOUND);
         }
 
         // Remove the configuration and save changes
         let removed_config = configs.configurations.remove(id - 1);
-        configs.save_to_file().await.map_err(|e| {
-            actix_web::error::ErrorInternalServerError(format!("Failed to save config: {}", e))
-        })?;
+        let _ = configs.save_to_file().map_err(|e| {
+            return HttpResponse::UnprocessableEntity()
+                .json(format!("Failed to save config: {}", e));
+        });
 
         removed_config
     };
 
     // Restart threads after deleting the configuration
-    manage_threads(&RequestType::Delete, thread_list.clone(), Data::from(Arc::clone(&data.configs))).await;
+    manage_threads(
+        &RequestType::Delete,
+        thread_list.clone(),
+        Data::from(Arc::clone(&data.configs)),
+    )
+    .await;
 
-    Ok(HttpResponse::Ok().json(removed_config))
+    HttpResponse::Ok().json(removed_config)
 }
 
-pub async fn get_actions_file(
-    data: web::Data<AppState>,
-    path: web::Path<usize>,
-) -> Result<impl Responder, Error> {
+#[get("/configurations/{id}/actions-file")]
+pub async fn get_actions_file(data: web::Data<AppState>, path: web::Path<usize>) -> impl Responder {
     let id = path.into_inner();
     let configs = data.configs.read().await;
 
@@ -193,9 +216,9 @@ pub async fn get_actions_file(
         let config = &configs.configurations[id - 1];
         let actions_file_content = std::fs::read_to_string(&config.actions_path)
             .unwrap_or_else(|_| "Actions file not found".to_string());
-        Ok(HttpResponse::Ok().body(actions_file_content))
+        HttpResponse::Ok().body(actions_file_content)
     } else {
-        Ok(HttpResponse::NotFound().body(CONFIG_NOT_FOUND))
+        HttpResponse::NotFound().body(CONFIG_NOT_FOUND)
     }
 }
 
