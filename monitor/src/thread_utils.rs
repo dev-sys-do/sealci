cbf1b9c70c0529e1e12fa6e3ca088980a53127aa
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-09-17 13:46:49 +0200

    feat(thread) : create thread only if events monitoring are requested
    Signed-off-by: Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>

--- monitor/src/thread_utils.rs
+++ monitor/src/thread_utils.rs
@@ -38,6 +38,12 @@ pub async fn create_thread(config: &SingleConfig, thread_list: &mut JoinSet<()>)
         .unwrap_or("https://controller.courtcircuits.xyz/pipeline".to_string());
 
     let repo_url = get_github_repo_url(&config.repo_owner, &config.repo_name);
-    thread_list.spawn(create_commit_listener(Arc::new(config.clone()), repo_url.clone(), Arc::new(controller_endpoint.clone())));
-    thread_list.spawn(create_pull_request_listener(Arc::new(config.clone()), repo_url, Arc::new(controller_endpoint)));
+    
+    if config.event == "commit" || config.event == "*" {
+        thread_list.spawn(create_commit_listener(Arc::new(config.clone()), repo_url.clone()));
+    }
+
+    if config.event == "pull request" || config.event == "*" {
+        thread_list.spawn(create_pull_request_listener(Arc::new(config.clone()), repo_url));
+    }
 }
