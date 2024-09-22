91c5a332c65997d524fef8041b9feb02f0700198
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-09-22 13:10:06 +0200

    feat(monitor): added better loging + env variable for controller + fixed request to controller

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,59 +1,78 @@
 mod config;
-mod event_listener;
 mod controller;
+mod event_listener;
 
 use crate::config::{Config, SingleConfig};
-use crate::event_listener::{get_github_repo_url, listen_to_commits, listen_to_pull_requests};
 use crate::controller::send_to_controller;
+use crate::event_listener::{get_github_repo_url, listen_to_commits, listen_to_pull_requests};
 use clap::{Arg, Command};
+use std::env;
+use std::error::Error;
 use std::path::Path;
 use std::sync::Arc;
 use tokio;
-use std::error::Error;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // CLI arguments
+    dotenv::dotenv().ok();
+    tracing_subscriber::fmt::init();
     let matches = Command::new("GitHub Monitor")
         .version("1.0")
         .about("Monitors a GitHub repository for changes")
-        .arg(Arg::new("config")
-            .short('c')
-            .long("config")
-            .required(false)
-            .help("The path to the config file"))
-        .arg(Arg::new("event")
-            .short('e')
-            .long("event")
-            .required(false)
-            .help("The event to listen to (commit, pull_request, *)"))
-        .arg(Arg::new("repo_owner")
-            .short('o')
-            .long("repo_owner")
-            .required(false)
-            .help("The owner of the repo to watch"))
-        .arg(Arg::new("repo_name")
-            .short('n')
-            .long("repo_name")
-            .required(false)
-            .help("The name of the repo to watch"))
-        .arg(Arg::new("github_token")
-            .short('t')
-            .long("github_token")
-            .required(false)
-            .help("The GitHub token"))
-        .arg(Arg::new("actions_path")
-            .short('a')
-            .long("actions_path")
-            .required(false)
-            .help("The path to the actions file"))
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .required(false)
+                .help("The path to the config file"),
+        )
+        .arg(
+            Arg::new("event")
+                .short('e')
+                .long("event")
+                .required(false)
+                .help("The event to listen to (commit, pull_request, *)"),
+        )
+        .arg(
+            Arg::new("repo_owner")
+                .short('o')
+                .long("repo_owner")
+                .required(false)
+                .help("The owner of the repo to watch"),
+        )
+        .arg(
+            Arg::new("repo_name")
+                .short('n')
+                .long("repo_name")
+                .required(false)
+                .help("The name of the repo to watch"),
+        )
+        .arg(
+            Arg::new("github_token")
+                .short('t')
+                .long("github_token")
+                .required(false)
+                .help("The GitHub token"),
+        )
+        .arg(
+            Arg::new("actions_path")
+                .short('a')
+                .long("actions_path")
+                .required(false)
+                .help("The path to the actions file"),
+        )
         .get_matches();
 
+    let controller_endpoint = env::var("CONTROLLER_ENDPOINT")
+        .unwrap_or("https://controller.courtcircuits.xyz/pipeline".to_string());
+
     let config: Config = if let Some(config_path) = matches.get_one::<String>("config") {
-        println!("-- SealCI - Loading config from file: {:?}", config_path);
+        info!("-- SealCI - Loading config from file: {:?}", config_path);
         Config::from_file(config_path)?
     } else {
-        println!("-- SealCI - Loading config from CLI arguments");
+        info!("-- SealCI - Loading config from CLI arguments");
 
         let repo_name = matches
             .get_one::<String>("repo_name")
@@ -79,7 +98,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         .get_one::<String>("actions_path")
                         .ok_or("--actions_path argument is required")?
                         .to_string();
-                    
+
                     // Validation de l'existence du fichier
                     Config::exists_actions_file(&path, repo_name)?;
                     path
@@ -88,8 +107,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    println!("-- SealCI - Config loaded !");
-    println!("{:#?}", config);
+    info!("-- SealCI - Config loaded !");
+    info!("{:#?}", config);
 
     let mut handles = vec![];
 
@@ -102,19 +121,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let commit_listener = {
             let config = Arc::clone(&config);
             let repo_url = repo_url.clone();
+            let controller_endpoint = controller_endpoint.clone();
 
             if config.event == "commit" || config.event == "*" {
                 Some(tokio::spawn(async move {
                     let callback = {
                         let config = Arc::clone(&config);
                         let repo_url = repo_url.clone();
+                        let controller_endpoint = controller_endpoint.clone();
                         move || {
                             let config = Arc::clone(&config);
                             let repo_url = repo_url.clone();
+                            let controller_endpoint = controller_endpoint.clone();
                             tokio::spawn(async move {
-                                match send_to_controller(&repo_url, Path::new(&config.actions_path)).await {
-                                    Ok(_) => println!("Pipeline sent successfully"),
-                                    Err(e) => eprintln!("Failed to send pipeline: {}", e),
+                                match send_to_controller(
+                                    &repo_url,
+                                    Path::new(&config.actions_path),
+                                    &controller_endpoint,
+                                )
+                                .await
+                                {
+                                    Ok(_) => info!("Pipeline sent successfully"),
+                                    Err(e) => error!("Failed to send pipeline: {}", e),
                                 }
                             });
                         }
@@ -131,19 +159,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let pull_request_listener = {
             let config = Arc::clone(&config);
             let repo_url = repo_url.clone();
+            let controller_endpoint = controller_endpoint.clone();
 
             if config.event == "pull_request" || config.event == "*" {
                 Some(tokio::spawn(async move {
                     let callback = {
                         let config = Arc::clone(&config);
                         let repo_url = repo_url.clone();
+                        let controller_endpoint = controller_endpoint.clone();
                         move || {
                             let config = Arc::clone(&config);
                             let repo_url = repo_url.clone();
+                            let controller_endpoint = controller_endpoint.clone();
                             tokio::spawn(async move {
-                                match send_to_controller(&repo_url, Path::new(&config.actions_path)).await {
-                                    Ok(_) => println!("Pipeline sent successfully"),
-                                    Err(e) => eprintln!("Failed to send pipeline: {}", e),
+                                match send_to_controller(
+                                    &repo_url,
+                                    Path::new(&config.actions_path),
+                                    &controller_endpoint,
+                                )
+                                .await
+                                {
+                                    Ok(_) => info!("Pipeline sent successfully"),
+                                    Err(e) => error!("Failed to send pipeline: {}", e),
                                 }
                             });
                         }
